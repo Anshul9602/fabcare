099bb9fbb0548141cf54030aba54e7c22b29199b
Anshul9602 <anshulkumar242507@gmail.com>
2024-01-19 11:12:39 +0530

    fab care main

--- config.php
+++ config.php
@@ -1,53 +1,16 @@
 <?php
 
-// HTTP
-define('HTTP_SERVER', 'https://solitairejewel.com/demo/');
-
-// HTTPS
-define('HTTPS_SERVER', 'https://solitairejewel.com/demo/');
-
-// DIR
-define('DIR_APPLICATION', '/home/webuser/web/solitairejewel.com/public_html/demo/catalog/');
-define('DIR_SYSTEM', '/home/webuser/web/solitairejewel.com/public_html/demo/system/');
-define('DIR_IMAGE', '/home/webuser/web/solitairejewel.com/public_html/demo/image/');
-define('DIR_STORAGE',  '/home/webuser/web/solitairejewel.com/public_html/demo/storage/');
-define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
-define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
-define('DIR_CONFIG', DIR_SYSTEM . 'config/');
-define('DIR_CACHE', DIR_STORAGE . 'cache/');
-define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
-define('DIR_LOGS', DIR_STORAGE . 'logs/');
-define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
-define('DIR_SESSION', DIR_STORAGE . 'session/');
-define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
-
-
-// DB
-define('DB_DRIVER', 'mysqli');
-define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'webuser_fab');
-define('DB_PASSWORD', '_=2m;/yHWmb1{L6%');
-define('DB_DATABASE', 'webuser_fabc');
-define('DB_PORT', '3306');
-define('DB_PREFIX', 'oc_');
-
-
-// OpenCart API
-define('OPENCART_SERVER', 'https://www.opencart.com/');
-
-
-
 // // HTTP
-// define('HTTP_SERVER', 'http://localhost/pe/');
+// define('HTTP_SERVER', 'https://solitairejewel.com/demo/');
 
 // // HTTPS
-// define('HTTPS_SERVER', 'http://localhost/pe/');
+// define('HTTPS_SERVER', 'https://solitairejewel.com/demo/');
 
 // // DIR
-// define('DIR_APPLICATION', 'C:/xampp/htdocs/pe/catalog/');
-// define('DIR_SYSTEM', 'C:/xampp/htdocs/pe/system/');
-// define('DIR_IMAGE', 'C:/xampp/htdocs/pe/image/');
-// define('DIR_STORAGE', 'C:/xampp/htdocs/pe/storage/');
+// define('DIR_APPLICATION', '/home/webuser/web/solitairejewel.com/public_html/demo/catalog/');
+// define('DIR_SYSTEM', '/home/webuser/web/solitairejewel.com/public_html/demo/system/');
+// define('DIR_IMAGE', '/home/webuser/web/solitairejewel.com/public_html/demo/image/');
+// define('DIR_STORAGE',  '/home/webuser/web/solitairejewel.com/public_html/demo/storage/');
 // define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
 // define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
 // define('DIR_CONFIG', DIR_SYSTEM . 'config/');
@@ -58,13 +21,50 @@ define('OPENCART_SERVER', 'https://www.opencart.com/');
 // define('DIR_SESSION', DIR_STORAGE . 'session/');
 // define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
 
+
 // // DB
 // define('DB_DRIVER', 'mysqli');
 // define('DB_HOSTNAME', 'localhost');
-// define('DB_USERNAME', 'root');
-// define('DB_PASSWORD', '');
-// define('DB_DATABASE', 'fab');
+// define('DB_USERNAME', 'webuser_fab');
+// define('DB_PASSWORD', '_=2m;/yHWmb1{L6%');
+// define('DB_DATABASE', 'webuser_fabc');
 // define('DB_PORT', '3306');
 // define('DB_PREFIX', 'oc_');
+
+
+// // OpenCart API
+// define('OPENCART_SERVER', 'https://www.opencart.com/');
+
+
+
+// HTTP
+define('HTTP_SERVER', 'http://localhost/fab/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/fab/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/fab/catalog/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/fab/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/fab/image/');
+define('DIR_STORAGE', 'C:/xampp/htdocs/fab/storage/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'webuser_fabc');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
 
 
